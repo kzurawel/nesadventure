acc584b23d608d626ea968b4b448e986d63a76ee
Kevin Zurawel <kzurawel@gmail.com>
2018-04-13 11:30:34 -0500

    ex0N -> exN

--- ex4-input/build.bat
+++ ex4-input/build.bat
@@ -2,4 +2,4 @@
 c:\cc65\bin\ca65 src\main.asm
 c:\cc65\bin\ca65 src\sprite.asm
 c:\cc65\bin\ca65 src\input.asm
-c:\cc65\bin\ld65 src\main.o src\sprite.o src\input.o -C nes.cfg -o ex04-input.nes
+c:\cc65\bin\ld65 src\main.o src\sprite.o src\input.o -C nes.cfg -o ex4-input.nes
--- ex5-backgrounds/build.bat
+++ ex5-backgrounds/build.bat
@@ -3,4 +3,4 @@ c:\cc65\bin\ca65 src\main.asm
 c:\cc65\bin\ca65 src\sprite.asm
 c:\cc65\bin\ca65 src\input.asm
 c:\cc65\bin\ca65 src\backgrounds.asm
-c:\cc65\bin\ld65 src\main.o src\sprite.o src\input.o src\backgrounds.o -C nes.cfg -o ex05-backgrounds.nes
+c:\cc65\bin\ld65 src\main.o src\sprite.o src\input.o src\backgrounds.o -C nes.cfg -o ex5-backgrounds.nes
